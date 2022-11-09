7bc5b9cd505b2a270a672f13e0ecdaf9f7803177
MatteoRicchiuto <116072651+MatteoRicchiuto@users.noreply.github.com>
2022-11-09 10:13:43 +0100

    Gues the word game

---
+++ Gues the word game.py
@@ -0,0 +1,12 @@
+i = 0
+secret_word = "ciao"
+gues = ""
+
+while i < 3:
+     gues = input("insert the word: ")
+     if gues == secret_word:
+        print("You win")
+        break
+     elif gues != secret_word and i < 3:
+        print("you lose")
+     i += 1
