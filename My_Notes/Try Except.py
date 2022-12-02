4702e455517213b04e590a301b32c219be7c66d8
MatteoRicchiuto <116072651+MatteoRicchiuto@users.noreply.github.com>
2022-12-02 10:38:23 +0100

    Update Try Except.py

--- My_Notes/Try Except.py
+++ My_Notes/Try Except.py
@@ -4,14 +4,14 @@ try:
 
     print('minore' if a < b else 'uguale' if a == b else 'maggiore')
 
-except ValueError :
-    
-    print('Non hai inserito un valore intero ')
+except ValueError :     
+    print('Non hai inserito un valore intero ')         # Viene stampato soltante se c'è un erorre di valore
 
 except NameError : 
-    
-    print('Torna alle elementari che non sai scrivere')
+    print('Torna alle elementari che non sai scrivere') # Viene stampato soltante se c'è un erorre di nome
 
 except:
-    
-    print("Errore generico")
+    print("Errore generico")                # Viene stampato per qualsiasi errore non specificat in precedenza
+
+finally:
+    print('Hope you got the result you want!')  # Viene stampato sempre alla fine del programma
